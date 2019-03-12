f98aea37f09d1a4c1520f47c1775280e9949d7c3
luweiDavid <daiwei@talkmoney.cn>
2019-03-12 19:22:29 +0800

    初始化项目

---
+++ Assets/Scripts/ResourcesTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using System.Text;
+using System.Xml.Serialization;
+
+public class ResourcesTest : MonoBehaviour {
+
+    private void Start()
+    {
+        SerilizeClassFunc();
+    }
+
+
+    public void SerilizeClassFunc() {
+        TestSerilize ts = new TestSerilize();
+        ts.Id = 1;
+        ts.Name = "test";
+        ts._intList = new List<int>();
+        ts._intList.Add(2);
+        ts._intList.Add(3);
+        XmlSerilizeFunc(ts);
+    } 
+
+    void XmlSerilizeFunc(TestSerilize tests) {
+        FileStream fs = new FileStream(Application.dataPath + "/test.xml", FileMode.Create, FileAccess.ReadWrite);
+        StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+        XmlSerializer xmls = new XmlSerializer(tests.GetType());
+        xmls.Serialize(sw, tests);
+        sw.Close();
+        fs.Close();
+    }
+}
---
+++ Assets/Scripts/TestSerilize.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+
+[System.Serializable]  //可序列标记
+public class TestSerilize {
+    [XmlAttribute("Id")]
+    public int Id;
+    [XmlAttribute("Name")]
+    public string Name;
+    [XmlElement("intList")]
+    public List<int> _intList;
+}
